c0fed487a5d72017b525c0b8a88136a6d698ddda
Sergey Vartanov <me@enzet.ru>
2014-12-30 14:18:22 +0300

    Style in CSS and PEP 8 formatting.

--- http_serv.py
+++ http_serv.py
@@ -9,15 +9,22 @@ import BaseHTTPServer
 class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     hconf = {
-            'user'     : {'title': 'User', 'type':'text'},
-            'filename' : {'title': 'Patch', 'type': 'file'},
-            'branch'   : {'title': 'Branch', 'type': 'radio', 'values': ['10.0', '10.1', 'trunk']},
-            'defects'  : {'title': 'Enable defects?', 'type': 'checkbox', 'values': ['yes']},
-            'buildspec': {'title': 'Create buildspec?', 'type': 'checkbox', 'values': ['yes']},
-            'build'    : {'title': 'Build', 'values': ['old', 'new']},
-            'system'   : {'title': 'Test systems', 'type': 'checkbox', 'values': ['EngineTest', 'T_and_V']},
-            'projects' : {'title': 'Projects', 'type': 'checkbox',
-                         'values': ['android-5.0.2_r1', 'boost_1_57_0', 'firefox-35', 'linux-3.18.1']}
+            'user': {'title': 'User', 'type':'text'},
+            'filename': {'title': 'Patch<br />file', 'type': 'file', 'style': 'tt'},
+            'branch': {'title': 'Branch', 'type': 'radio',
+                       'values': {'10.0': {}, '10.1': {}, 'trunk': {'caption': '#trunk'}}},
+            'defects': {'title': 'Enable<br />defects?', 'type': 'checkbox', 'values': {'yes': {'caption': 'Yes'}}},
+            'buildspec': {'title': 'Create<br />buildspec?', 'type': 'checkbox', 'values': {'yes': {'caption': 'Yes'}}},
+            'build': {'title': 'Build', 'values': {'old': {}, 'new': {}}},
+            'system': {'title': 'Test<br />systems', 'type': 'checkbox',
+                       'values': {'EngineTest': {'caption': 'Engine Test'}, 'T_and_V': {'caption': 'T and V'}}},
+            'projects': {'title': 'Projects', 'type': 'checkbox',
+                         'values': {
+                             'android-5.0.2_r1': {'caption': 'Android#5.0.2 R1'},
+                             'boost_1_57_0': {'caption': 'Boost#1.57.0'},
+                             'firefox-35': {'caption': 'Firefox#35'},
+                             'linux-3.18.1': {'caption': 'Linux#3.18.1'},
+                         }}
             }
 
     def get_log(self):
@@ -43,16 +50,27 @@ class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type',    'text/html')
         self.end_headers()
-        buf = '<html><body><h1>Killing '+ doc['filename'] +'</h1></body></html>'
+        buf = '<html><body><h1>Killing ' + doc['filename'] + '</h1></body></html>'
         self.wfile.write(buf)
 
+    @staticmethod
+    def format_tag(text):
+        """
+        Format string for HTML representation.
+
+        :param text: input string
+        :return formatted string
+        """
+        if '#' in text:
+            text = text[:text.find('#')] + ' <span class = tag>' + text[text.find('#') + 1:] + '</span>'
+        return text.strip()
 
     def construct_result_table(self):
-        table = '<table border=1"><tr>'
+        table = '<table class = main>'
 
         couch = couchdb.Server()
         db = couch['patches']
-        docs = [ db[idx] for idx in db ]
+        docs = [db[idx] for idx in db]
         
         # head
         table += '<tr>'
@@ -61,58 +79,103 @@ class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 table += '<th>' + v['title'] + '</th>'
         table += '</tr>'
         
+        index = 0
+        display_items = 1
+
         # cells
         for doc in reversed(docs):
             table += '<tr>'
             for k, v in sorted(self.hconf.items(), key=lambda x: x[1]):
                 if 'type' in v:
-                    table += '<td>' + str(doc[k]) + '</td>'
+                    if isinstance(doc[k], list):
+                        td = ''
+                        for item in doc[k][:display_items]:
+                            if 'values' in self.hconf[k] and 'caption' in self.hconf[k]['values'][str(item)]:
+                                td += self.format_tag(self.hconf[k]['values'][item]['caption']) + '<br />'
+                            else:
+                                td += item + '<br />'
+                        if len(doc[k]) > 2:
+                            index += 1
+                            td += '<span id = "hidden' + str(index) + '" style = "display: none">'
+                            for item in doc[k][display_items:]:
+                                if 'values' in self.hconf[k] and 'caption' in self.hconf[k]['values'][str(item)]:
+                                    td += self.format_tag(self.hconf[k]['values'][item]['caption']) + '<br />'
+                                else:
+                                    td += item + '<br />'
+                            td += '</span><div><span class = "more" id = "more' + str(index) + \
+                                  '" onclick = "myFunction(' + str(index) + ')">more</span></div>'
+                    else:
+                        td = str(doc[k])
+                    if 'style' in v:
+                        td = '<span class = ' + v['style'] + '>' + td + '</span>'
+                    table += '<td>' + td + '</td>'
             table += '</tr>'
         table += '</table>'
         return table
 
-
     def construct_input_form(self):
         input_form = '<h2>Send patch to testing machine</h2>'
-        input_form += '<form action=/ method=POST ENCTYPE=multipart/form-data>'
+        input_form += '<div class = send><form action=/ method=POST ENCTYPE=multipart/form-data>'
+        input_form += '<table class = parameters>'
 
-        for k,v in sorted(self.hconf.items(), key=lambda x: x[1]):
+        for k, v in sorted(self.hconf.items(), key=lambda x: x[1]):
             if 'type' not in v:
                 continue
 
-            input_form += '<fieldset><legend>' + v['title'] + '</legend>'
+            input_form += '<tr><td class = label>' + v['title'] + '</td><td>'
 
             if 'values' in v:
                 for val in v['values']:
                     name = k if v['type'] == 'radio' else k + val
                     value = val if v['type'] == 'radio' else 'enabled'
 
-                    input_form += '<input type=' + v['type'] + ' name=' + name + ' value=' + value + ' />' + val + '<br/>'
+                    caption = self.format_tag(v['values'][val]['caption']) if 'caption' in v['values'][val] else val
+
+                    input_form += '<p class = ti><label class = ' + v['type'] + '><input type=' + v['type'] + \
+                                  ' name=' + name + ' value=' + value + ' /><span class = branch>' + caption + \
+                                  '</span></label></p>'
             else:
                 input_form += '<input type=' + v['type'] + ' name=' + k + ' />'
 
-            input_form += '</fieldset>'
+            input_form += '</td></tr>'
 
-        input_form += '<p><input type=submit value=Upload></p>'
-        input_form += '</form>'
+        input_form += '<tr><td></td><td><input type=submit value="Upload patch"></td></tr>'
+        input_form += '</table>'
+        input_form += '</form></div>'
         return input_form
-
 
     def get_default(self):
         result_table = self.construct_result_table()
         input_form = self.construct_input_form()
 
-        message = '<html><head><title>Upload</title></head><body>\
-                     <table><tr><td style="padding:10px;vertical-align:top;">'
+        message = '<html><head><title>Upload</title><link rel = "stylesheet" type = "text/css" href = "style.css">\
+                     </head><body><table><tr><td style="padding:10px;vertical-align:top;">'
         message += input_form
-        message += '</td><td style="padding:10px;vertical-align:top;">'
+        message += '</td><td><div class = table>'
         message += result_table
-        message += '</td></tr></table></body></html>'
+        message += '</div></td></tr></table>'
+        message += '''<script>
+function myFunction(number) {
+    var text = document.getElementById("hidden" + number);
+    var more = document.getElementById("more" + number);
+    if (text.style.display == "none")
+    {
+        text.style.display = "block";
+        more.innerHTML = "hide";
+    }
+    else
+    {
+        text.style.display = "none";
+        more.innerHTML = "more";
+    }
+}
+</script>
+'''
+        message += '</body></html>'
 
         self.send_response(200)
         self.end_headers()
         self.wfile.write(message)
-
 
     def do_GET(self):
         if self.path.endswith(".log") or self.path.endswith("realtime"):
@@ -122,13 +185,12 @@ class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         else:
             self.get_default()
 
-
     def do_POST(self):
         form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
-                                environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'], })
+                                environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
         doc = {}
 
-        for k,v in self.hconf.items():
+        for k, v in self.hconf.items():
             doc[k] = None
 
             if 'type' not in v:
@@ -152,8 +214,8 @@ class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     filename = upfile.filename.split('\\')[-1]
                 else:
                     filename = upfile.filename.split('/')[-1]
-                filename = re.sub('[ \t]','-', filename)
-                filename = re.sub('[^a-zA-Z0-9_.:-]','', filename)
+                filename = re.sub('[ \t]', '-', filename)
+                filename = re.sub('[^a-zA-Z0-9_.:-]', '', filename)
 
                 fp = open(filename, 'wb')
                 while True:
@@ -161,27 +223,25 @@ class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     if len(chunk) == 0:
                         break
                     else:
-                        fp.write(chunk)
+                        fp.write(chunk)ddi
                 fp.close()
                 doc[k] = filename
 
         self.send_response(200)
         self.end_headers()
 
-        self.wfile.write('<html><head><title>Upload</title></head><body>' + str(doc)+ '</body></html>')
+        self.wfile.write('<html><head><title>Upload</title></head><body>' + str(doc) + '</body></html>')
 
         couch = couchdb.Server()
         db = couch['patches']
         db.save(doc)
 
-
 if __name__ == '__main__':
     try:
-        port = 8080 #random.randint(50000,60000)
-        url = "http://adumu:%d/" % (port)
-        server = BaseHTTPServer.HTTPServer( ('', port), GetHandler )
+        port = 8080  # random.randint(50000,60000)
+        url = "http://adumu:%d/" % port
+        server = BaseHTTPServer.HTTPServer(('', port), GetHandler)
         print "Ask user to visit this URL:\n\t%s" % url
         server.serve_forever()
     except KeyboardInterrupt:
         pass
-
